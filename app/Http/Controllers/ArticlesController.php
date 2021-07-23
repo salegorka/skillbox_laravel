7712dd7004b43b587fad39189ff941768ee90b86
salegorka <egorsalomatin567@gmail.com>
2021-07-23 14:12:46 +0300

    Задание 5. Доработан ресурс статьи.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 use App\Models\Article;
 class ArticlesController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
+        $notification = "";
+        if ($request->has('updated')) {
+            $art = Article::where('slug', '=', $request->updated)->first();
+            $notification = "Статья с названием " . $art->name . " обновлена.";
+        }
+        if ($request->has('created')) {
+            $art = Article::where('slug', '=', $request->created)->first();
+            $notification = "Статья с названием " . $art->name . " создана.";
+        }
+        if ($request->has('deleted')) {
+            $notification = "Статья с названием " . $request->deleted . " удалена.";
+        }
         $articles = Article::where('published', 1)->latest()->get();
-        return view('main', compact('articles'));
+        return view('main', compact('articles', 'notification'));
     }
 
     public function show(Article $article)
@@ -23,26 +36,50 @@ class ArticlesController extends Controller
         return view('articles.create');
     }
 
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
-        $request->validate([
-            'slug' => ['required','unique:articles', 'regex:/^[a-zA-Z0-9_-]+$/i'],
-            'name' => 'required|min:5|max:255',
-            'short-description' => 'required|max:255',
-            'description' => 'required'
-        ]);
-        $data = $request->all();
-        $article = new Article();
-        $article->slug = $data['slug'];
-        $article->name = $data['name'];
-        $article->short_description = $data['short-description'];
-        $article->description = $data['description'];
+        $data = $request->validated();
+
         if (isset($data['published']) && $data['published'] == 'on') {
-            $article->published = true;
+            $published = true;
         } else {
-            $article->published = false;
+            $published = false;
         }
-        $article->save();
-        return redirect('/');
+       $article = Article::create(['slug' => $data['slug'],
+            'name' => $data['name'],
+            'short_description' => $data['short-description'],
+            'description' => $data['description'],
+            'published' => $published]);
+        return redirect('/?created=' . $article->slug);
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $data = $request->validated();
+        if (isset($data['published']) && $data['published'] == 'on') {
+            $published = true;
+        } else {
+            $published = false;
+        }
+        $article->update([
+            'slug' => $data['slug'],
+            'name' => $data['name'],
+            'short_description' => $data['short-description'],
+            'description' => $data['description'],
+            'published' => $published
+        ]);
+        return redirect('/?updated=' . $article->slug);
+    }
+
+    public function destroy(Article $article)
+    {
+        $name = $article->name;
+        $article->delete();
+        return redirect('/?deleted=' . $name);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\FeedbackController;
 Route::get('/', [ArticlesController::class, 'index']);
 Route::get('/articles/create', [ArticlesController::class, 'create']);
-Route::get('/articles/{article:slug}', [ArticlesController::class, 'show']);
+Route::get('/articles/{article}', [ArticlesController::class, 'show']);
+Route::get('/articles/{article}/edit', [ArticlesController::class, 'edit']);
 Route::post('/articles', [ArticlesController::class, 'store']);
+Route::patch('/articles/{article}', [ArticlesController::class, 'update']);
+Route::delete('/articles/{article}', [ArticlesController::class, 'destroy']);
+
 Route::get('/admin/feedback', [FeedbackController::class, 'index']);
 Route::get('/contacts', [FeedbackController::class, 'create']);
 Route::post('/contacts', [FeedbackController::class, 'store']);
